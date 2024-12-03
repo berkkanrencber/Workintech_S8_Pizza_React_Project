5cdbb086fa9a9634ada13251fe647c4caeaf3db8
Berkkan Rençber <berkkanrencber@gmail.com>
2024-12-03 12:39:23 +0300

    Project was created. Template was supplied.

---
+++ teknolojik-yemekler/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from "./pages/Home";
+import OrderForm from "./pages/OrderForm";
+import Success from "./pages/Success";
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<OrderForm />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
